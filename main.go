e6a6650dd62e270560826764cb8d21cd7f479413
jxd3700 <jxd3700@g.rit.edu>
2019-11-24 19:39:12 -0500

    first function added with helper functions

--- main.go
+++ main.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"strconv"
 )
 
-func SingleHistoricalFreq(freq float64, impact int) float64 {
-	return 0.0
+func Headings(wrkbook *excelize.File, row string) {
+	curcell := ""
+	i := 0
+	for i = 0; i < 2; i++ {
+		curcell = row + strconv.Itoa(i+1)
+		wrkbook.SetCellValue("Sheet1", curcell, 2.02)
+	}
+	curcell = row + strconv.Itoa(i+1)
+	formula := TwoCellFormula("Sum", row+strconv.Itoa(1), row+strconv.Itoa(2))
+	wrkbook.SetCellFormula("Sheet1", curcell, formula)
+}
+
+func TwoCellFormula(formulaType, firstCell, lastCell string) string {
+	return formulaType + "(" + firstCell + ":" + lastCell + ")"
+}
+
+func SingleHistoricalFreq(freq float64, impact int) string {
+	return ""
 }
 
 func main() {
 
 	wrkbook := excelize.NewFile()
 	wrkbook.NewSheet("Sheet1")
-	wrkbook.SetCellValue("Sheet1", "A1", 2.02)
-	wrkbook.SetCellValue("Sheet1", "A2", 1.01)
-	wrkbook.SetCellFormula("Sheet1", "A3", "SUM(A1:A2)")
+	Headings(wrkbook, "A")
 	err := wrkbook.SaveAs("C:/Users/johnn/Documents/projects-yugioh/datasheets/blanksheet.xlsx")
 	if err != nil {
 		fmt.Println(err)
