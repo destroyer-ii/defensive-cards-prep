9030cdc61f4a20ab0516fc8413f6732d93e1d937
jxd3700 <jxd3700@g.rit.edu>
2019-11-25 22:59:49 -0500

    added skeleton functions

--- main.go
+++ main.go
@@ -6,21 +6,31 @@ import (
 	"strconv"
 )
 
-type Mapping struct {
-	CardNames []string
-	DeckNames []string
-}
+var CardNames []string
+var DeckNames []string
 
-func Headings(wrkbook *excelize.File, row string) {
+var lettermapping map[int]string
+
+//TODO: CHANGE THIS TO WORK WITH ROW INSTEAD OF COLUMN
+func Headings(wrkbook *excelize.File, column string) {
+	//start from E, go to end of thing
 	curcell := ""
 	i := 0
 	for i = 0; i < 2; i++ {
-		curcell = row + strconv.Itoa(i+1)
+		curcell = column + strconv.Itoa(i+1)
 		wrkbook.SetCellValue("Sheet1", curcell, 2.02)
 	}
-	curcell = row + strconv.Itoa(i+1)
-	formula := TwoCellFormula("Sum", row+strconv.Itoa(1), row+strconv.Itoa(2))
+	curcell = column + strconv.Itoa(i+1)
+	formula := TwoCellFormula("Sum", column+strconv.Itoa(1), column+strconv.Itoa(2))
 	wrkbook.SetCellFormula("Sheet1", curcell, formula)
+}
+
+func Matchups(wrkbook *excelize.File, row int) {
+	//set up matchups in B column
+}
+
+func ScoreVMean(wrkbook *excelize.File, row int) {
+	//set up score vs mean in last column before moving on to next section
 }
 
 func TwoCellFormula(formulaType, firstCell, lastCell string) string {
